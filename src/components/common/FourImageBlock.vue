fbb50bf5ec3651b5884863afd746c0515da956ca
Adarsh T <adarshtcreation@gmail.com>
2024-07-21 22:47:52 +0530

    All section changes

--- src/components/common/FourImageBlock.vue
+++ src/components/common/FourImageBlock.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container fluid>
-    <v-row>
+    <v-row v-if="mdAndUp">
       <v-col cols="8">
         <v-row>
           <v-col cols="12"><ImageBlock :block="subBlocks[0]" /> </v-col>
@@ -12,10 +12,18 @@
         <ImageBlock :block="subBlocks[3]" />
       </v-col>
     </v-row>
+
+    <v-row v-else>
+      <v-col cols="12"><ImageBlock :block="subBlocks[0]" /> </v-col>
+      <v-col cols="12"><ImageBlock :block="subBlocks[1]" /> </v-col>
+      <v-col cols="12"><ImageBlock :block="subBlocks[2]" /> </v-col>
+      <v-col cols="12"><ImageBlock :block="subBlocks[3]" /> </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
+import { useDisplay } from "vuetify";
 import ImageBlock from "@/components/common/ImageBlock.vue";
 export default {
   name: "FourImageBlock",
@@ -28,6 +36,13 @@ export default {
       required: false,
       default: () => [],
     },
+  },
+  setup() {
+    const { mdAndUp } = useDisplay();
+
+    return {
+      mdAndUp,
+    };
   },
 };
 </script>
--- src/components/common/ThreeImageBlock.vue
+++ src/components/common/ThreeImageBlock.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container fluid>
-    <v-row>
+    <v-row v-if="mdAndUp">
       <v-col cols="8">
         <ImageBlock :block="subBlocks[0]" />
       </v-col>
@@ -10,6 +10,11 @@
           <v-col cols="12"><ImageBlock :block="subBlocks[2]" /> </v-col>
         </v-row>
       </v-col>
+    </v-row>
+    <v-row v-else>
+      <v-col cols="12"><ImageBlock :block="subBlocks[0]" /></v-col>
+      <v-col cols="12"><ImageBlock :block="subBlocks[1]" /></v-col>
+      <v-col cols="12"><ImageBlock :block="subBlocks[2]" /></v-col>
     </v-row>
   </v-container>
 </template>
