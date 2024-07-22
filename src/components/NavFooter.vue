2ea99212ed019dfb4a32f0b28b17a0ac45a079bd
Adarsh T <adarshtcreation@gmail.com>
2024-07-22 06:03:09 +0530

    All section changes

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -2,7 +2,7 @@
   <v-footer>
     <v-container fluid>
       <v-row class="hidden-sm-and-down">
-        <v-col cols="2" class="text-left">
+        <v-col cols="2" class="text-left align-center d-flex">
           <img alt="logo" :src="require('../assets/logo.png')" height="50" />
         </v-col>
 
@@ -16,22 +16,23 @@
               :key="link.id"
               class="text-left"
             >
-              <h4>{{ link.label }}</h4>
+              <h4 class="pl-4 pr-4">{{ link.label }}</h4>
               <v-list dense>
-                <v-list-item
+                <router-link
                   v-for="item in link.menuItems"
                   :key="item.id"
-                  link
-                  class="pl-0 pr-0"
+                  :to="item.url"
+                  class="text-decoration-none text-black"
+                  ><v-list-item link class="text-caption">
+                    {{ item.label }}
+                  </v-list-item></router-link
                 >
-                  {{ item.label }}
-                </v-list-item>
               </v-list>
             </v-col>
           </v-row>
         </v-col>
 
-        <v-col cols="2" class="d-flex">
+        <v-col cols="2" class="d-flex pt-0">
           <v-btn
             v-for="icon in icons"
             :key="icon"
@@ -62,14 +63,15 @@
           </v-expansion-panel-title>
           <v-expansion-panel-text class="base-primry-black">
             <v-list class="text-left pa-0">
-              <v-list-item
+              <router-link
                 v-for="item in link.menuItems"
                 :key="item.id"
-                link
-                class="text-caption pa-0 base-primry-black"
+                :to="item.url"
+                class="text-decoration-none text-black"
+                ><v-list-item link class="text-caption pa-0 base-primry-black">
+                  {{ item.label }}
+                </v-list-item></router-link
               >
-                {{ item.label }}
-              </v-list-item>
             </v-list>
           </v-expansion-panel-text>
         </v-expansion-panel>
--- src/components/common/ImageBlock.vue
+++ src/components/common/ImageBlock.vue
@@ -1,46 +1,66 @@
 <template>
   <v-parallax
     :src="require(`@/assets/${block.image}`)"
-    class="text-white"
+    class="text-white custom-parallax"
     height="100%"
   >
-    <v-row class="fill-height" align="center" justify="center">
-      <v-col v-if="singleBlock" class="text-left" cols="12" md="8">
-        <div class="text-content">
-          <p class="text-h2">{{ block.title }}</p>
-          <p class="text-h6">
-            {{ block.body }}
-          </p>
-        </div>
-      </v-col>
-
-      <v-col v-else class="text-left" cols="12" md="10">
-        <div class="text-content">
-          <p class="text-subtitle-1 text-uppercase font-weight-bold">
-            {{ block.title }}
-          </p>
-          <p class="text-subtitle-2 text-uppercase font-weight-thin">
-            {{ block.subtitle }}
-          </p>
-          <p class="text-body mt-6">
-            {{ block.body }}
-          </p>
-          <div class="mt-6">
-            <router-link
-              :to="block.url"
-              class="text-decoration-none router-btn"
+    <v-hover>
+      <template v-slot:default="{ isHovering, props }">
+        <v-row
+          class="fill-height custom-parallax-content"
+          :class="[
+            singleBlock ? 'single-block' : '',
+            isHovering ? 'hovered' : '',
+          ]"
+          align="center"
+          justify="center"
+        >
+          <v-col v-if="singleBlock" class="text-left" cols="12" md="8">
+            <div
+              class="text-content"
+              v-bind="props"
+              :class="isHovering ? '' : 'text-transparent'"
             >
-              <v-btn
-                variant="outlined"
-                class="rounded-0 text-overline text-uppercase"
-              >
-                View Project
-              </v-btn>
-            </router-link>
-          </div>
-        </div>
-      </v-col>
-    </v-row>
+              <p class="text-h2">{{ block.title }}</p>
+              <p class="text-h6 mt-4">
+                {{ block.body }}
+              </p>
+            </div>
+          </v-col>
+
+          <v-col v-else class="text-left" cols="12" md="10">
+            <div
+              class="text-content"
+              v-bind="props"
+              :class="isHovering ? '' : 'text-transparent'"
+            >
+              <p class="text-subtitle-1 text-uppercase font-weight-bold">
+                {{ block.title }}
+              </p>
+              <p class="text-subtitle-2 text-uppercase font-weight-thin">
+                {{ block.subtitle }}
+              </p>
+              <p class="text-body mt-6">
+                {{ block.body }}
+              </p>
+              <div class="mt-6">
+                <router-link
+                  :to="block.url"
+                  class="text-decoration-none router-btn"
+                >
+                  <v-btn
+                    variant="outlined"
+                    class="rounded-0 text-overline text-uppercase"
+                  >
+                    View Project
+                  </v-btn>
+                </router-link>
+              </div>
+            </div>
+          </v-col>
+        </v-row>
+      </template>
+    </v-hover>
   </v-parallax>
 </template>
 
@@ -65,5 +85,11 @@ export default {
 <style lang="scss" scoped>
 .router-btn {
   color: inherit;
+}
+.custom-parallax-content.single-block {
+  background-image: linear-gradient(rgb(0 0 0/40%) 0 0) !important;
+}
+.custom-parallax-content.hovered {
+  background-image: linear-gradient(rgb(0 0 0/40%) 0 0) !important;
 }
 </style>
